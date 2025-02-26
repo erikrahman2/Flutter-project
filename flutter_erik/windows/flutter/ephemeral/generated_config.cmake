22f81d8432d037fcdb49f96134c7285361f65a3b
erik rahman <118335576+erikrahman2@users.noreply.github.com>
2025-02-26 13:43:23 +0700

    Add files via upload

---
+++ flutter_erik/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\fluproject\\flutter_erik" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter\\flutter"
+  "PROJECT_DIR=D:\\fluproject\\flutter_erik"
+  "FLUTTER_ROOT=D:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\fluproject\\flutter_erik\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\fluproject\\flutter_erik"
+  "FLUTTER_TARGET=D:\\fluproject\\flutter_erik\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\fluproject\\flutter_erik\\.dart_tool\\package_config.json"
+)
